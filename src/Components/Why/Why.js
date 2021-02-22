9fd5e366f9e1c2bd52b4e5516df03a3dd6f1edf6
George Bjork <georgebjork@outlook.com>
2021-02-22 11:44:39 -0800

    why page is updated to full react bootstrap

--- src/Components/Why/Why.js
+++ src/Components/Why/Why.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
 
 export const Why = () => (
     <>
-        <div class="container col-10">
-            <div class="text-muted p-3">
+        <Container className="col-10">
+            <Container fluid className="text-muted p-3">
                 <h1 class="display-1 text-muted text-center mt-5 mb-5">Why Are We Making This App?</h1>
                 <p class="text-muted h-6 lead">
                     WhitTasks is an app that George Bjork and Madeline Schiefelbein are developing to provide students a schedule that fits their needs. Without WhitTasks, students don't
@@ -13,14 +14,14 @@ export const Why = () => (
                     can add these into a checklist feature of the app that would allow them to check off each of these “tasks” as they are completed. To add to this feature, professors would also be
                     able to access the assignments portion of the app and add in tasks for the student to complete. Professors would also be able to send out notifications to students, such as the
                     cancelation of class.
-            </p>
-            </div>
+                </p>
+            </Container>
 
-
-            <div class="text-white bg-dark mb-5">
+            <Container fluid className="text-white bg-dark mb-5">
+            
                 <h1 class="display-1 text-center p-5">We Heard Your Voices</h1>
 
-                <div class="p-5">
+                <Container fluid className="p-5">
 
                     <p class="lead">Q: What features intrigue you most about WhitTasks? </p>
                     <p class="display-6 fst-italic">"I like the idea of consolidating all of my school related schedule things into one platform."</p>
@@ -42,9 +43,9 @@ export const Why = () => (
                     <p class="display-6 fst-italic"> "Yes. I don’t love it but I don’t hate it, I mainly use it out of necessity." </p>
                     <p class="lead">- Savannah Hayward (Communications) </p>
 
-                </div>
-            </div>
+                </Container>
+            </Container>
 
-        </div>
+        </Container>
     </>
 )
